af6cc07d2a7e97df1ea0409e8e1999e1a557e3ef
Vicente Noguez <v.noguez@ec-systems.de>
2023-07-11 11:56:05 -0300

    first commit

---
+++ src/main/java/Loader.java
@@ -0,0 +1,14 @@
+public class Loader {
+	public Loader() {
+	}
+
+//	ToDo: created to check qodana
+	public String pluginLoaded() {
+		String pluginName = "default";
+		if (pluginName == null) {
+			pluginName = "default";
+		}
+
+		return null;
+	}
+}
